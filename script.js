c7c1338073abb8e5c6d5926b4f428aca81d493b4
httpedroluiz <pedroluizmizael@gmail.com>
2023-01-21 14:03:09 -0300

    Added function add and save

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-const form = document.querySelector('#form-habits')
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-    run: ['01-18', '01-19', '01-20'],
-    water: ['01-19', '01-20'],
-    exercises: ['01-20', '01-21'],
-    study: ['01-18']
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+    const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if (dayExists) {
+        alert("O dia já foi adicionado ⛔")
+        return
+    }
+
+    alert("Adicionado com sucesso ✅")
+    nlwSetup.addDay(today)
 }
+
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
